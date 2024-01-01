a6f6f1a2aa8e398b1c875b0b6313f76e33e28871
glowredman <fredcraft00@gmail.com>
2024-01-01 19:09:13 +0100

    Optionally set `/wiki` as safe directory (default off)

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,11 @@ if [ ! -d "/github/workspace/${INPUT_WIKI_FOLDER}" ]; then
 fi
 cp -a ${INPUT_WIKI_FOLDER}/. /wiki
 
+if [ "${INPUT_IGNORE_SAFE_WARNINGS}" = "true" ]; then
+    chown -R $USER:$USER /wiki
+    git config --global --add safe.directory /wiki
+fi
+
 echo "Git Config..."
 echo "-> User: ${INPUT_COMMIT_USERNAME}"
 echo "-> Email: ${INPUT_COMMIT_EMAIL}"
